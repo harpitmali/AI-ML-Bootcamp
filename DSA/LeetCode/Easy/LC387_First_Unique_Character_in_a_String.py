a41a83608d8642050c0af78040616234ced40bb3
Harpit Mali <harpitmali21@gmail.com>
2026-08-16 21:57:30 +0530

    Complete Day 26 regression evaluation scaling and hash map

---
+++ DSA/LeetCode/Easy/LC387_First_Unique_Character_in_a_String.py
@@ -0,0 +1,15 @@
+def firstUniqChar(s):
+        freq = {}
+
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
+
+        for i, char in enumerate(s):
+            if freq[char] == 1:
+                return i
+
+        return -1
+
+s = "leetcode"
+
+print(firstUniqChar(s))

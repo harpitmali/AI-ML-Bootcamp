7f64964aec0a5f018612673acbd82555c69730fd
Harpit Mali <harpitmali21@gmail.com>
2026-08-17 18:24:49 +0530

    Complete Day 27 classification and hash map patterns

---
+++ DSA/LeetCode/Medium/LC49_Group_Anagram.py
@@ -0,0 +1,16 @@
+def group_anagrams(strs):
+    groups = {}
+
+    for word in strs:
+        key = "".join(sorted(word))
+
+        if key not in groups:
+            groups[key] = []
+
+        groups[key].append(word)
+
+    return list(groups.values())
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+print(group_anagrams(strs))

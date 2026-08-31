93e4e09fd2c8421d67b579c1e980f4d862803c41
Harpit Mali <harpitmali21@gmail.com>
2026-08-31 11:33:02 +0530

    Complete Day 39 model tuning and final selection

--- DSA/LeetCode/practice.py
+++ DSA/LeetCode/practice.py
@@ -1,40 +1,16 @@
-def three_sum(nums):
-    nums.sort()
-    result = []
+def remove_duplicates(nums):
+    
+    slow = 0
 
-    for i in range(len(nums)):
+    for fast in range(1, len(nums)):
 
-        if i > 0 and nums[i] == nums[i - 1]:
-            continue
+        if nums[fast] != nums[slow]:
 
-        left = i + 1
-        right = len(nums) - 1
+            slow += 1
+            nums[slow] = nums[fast]
 
-        while left < right:
+    return slow + 1
 
-            current = nums[i] + nums[left] + nums[right]
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 
-            if current == 0:
-
-                result.append([nums[i], nums[left], nums[right]])
-
-                while left < right and nums[left] == nums[left+1]:
-                    left += 1
-                
-                while left < right and nums[right] == nums[right-1]:
-                    right -= 1
-
-                left += 1
-                right -= 1
-
-            elif current < 0:
-                left += 1
-
-            else:
-                right -= 1
-
-    return result
-
-nums = [-1, 0, 1, 2, -1, -4]
-
-print(three_sum(nums))
+print(remove_duplicates(nums))
